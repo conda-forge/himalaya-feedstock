4e23ccd51b0b74c1f4cc58ba00c750ab54e9e6d4
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-12 22:44:00 -0800

    fix windows

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,6 +1,8 @@
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
 
+copy %BUILD_PREFIX%\Library\lib\libssh2.lib %BUILD_PREFIX%\Library\lib\ssh2.lib
+
 :: check licenses
 cargo-bundle-licenses ^
     --format yaml ^
